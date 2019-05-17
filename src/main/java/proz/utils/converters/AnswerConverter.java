493821943de522be24a2b234a2228db19bc4e690
mal-sokol <malgorzata@e-sokol.pl>
2019-05-17 20:00:07 +0200

    zmiany w AnswerConverter

--- src/main/java/proz/utils/converters/AnswerConverter.java
+++ src/main/java/proz/utils/converters/AnswerConverter.java
@@ -18,18 +18,17 @@ public class AnswerConverter
         fxModel.setAnswer(answer.getAnswer());
         fxModel.setAnswerId(answer.getAnswerId());
         fxModel.setIsCorrect(answer.isCorrect());
-        fxModel.setQuestionId(answer.getQuestionId().getQuestionId();
+        fxModel.setQuestionId(answer.getQuestionId().getQuestionId());
         return fxModel;
     }
 
     public static Answer answerFxToAnswer(AnswerFxModel fxModel) throws ApplicationException, SQLException {
         Answer answer = new Answer();
-        Question question = new Question();
         QuestionDao dao = new QuestionDao();
         answer.setAnswer(fxModel.getAnswer());
         answer.setAnswerId(fxModel.getAnswerId());
         answer.setCorrect(fxModel.isIsCorrect());
-        question = dao.getQueryBuilder(Question.class).query().get(fxModel.getQuestionId());
+        Question question = dao.getQueryBuilder(Question.class).query().get(fxModel.getQuestionId());
         answer.setQuestionId(question);
         return answer;
     }
