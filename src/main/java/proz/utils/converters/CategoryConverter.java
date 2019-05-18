107eab27166ec4eb8ccc49014810ea06aeb92d0e
mal-sokol <malgorzata@e-sokol.pl>
2019-05-18 19:30:55 +0200

    Revert "Revert "convertery testów i kategorii""
    This reverts commit e4e8f0a8e95f41483a182d80218d3161ead1e57e.

--- src/main/java/proz/utils/converters/CategoryConverter.java
+++ src/main/java/proz/utils/converters/CategoryConverter.java
@@ -1,18 +1,24 @@
 package proz.utils.converters;
 
+import proz.database.models.Category;
 import proz.models.CategoryFxModel;
 
 public class CategoryConverter
 {
     private CategoryConverter() {}
 
-    public static CategoryFxModel categoryToCategoryFx()
+    public static CategoryFxModel categoryToCategoryFx(Category category)
     {
-
+        CategoryFxModel fxModel = new CategoryFxModel(category.getName(), category.getCategoryId(), null);
+        return fxModel;
     }
 
-    public static Category categoryFxToCategory()
+    public static Category categoryFxToCategory(CategoryFxModel fxModel)
     {
-
+        Category category = new Category();
+        category.setName(fxModel.getCategoryName());
+        category.setCategoryId(fxModel.getCategoryId());
+//        category.setTests();
+        return category;
     }
 }
--- src/main/java/proz/utils/converters/TestConverter.java
+++ src/main/java/proz/utils/converters/TestConverter.java
@@ -1,18 +1,26 @@
 package proz.utils.converters;
 
+import proz.database.models.Test;
 import proz.models.TestFxModel;
 
 public class TestConverter
 {
     private TestConverter() {}
 
-    public static TestFxModel testToTestFx()
+    public static TestFxModel testToTestFx(Test test)
     {
-
+        TestFxModel fxModel = new TestFxModel();
+        fxModel.setTestId(test.getTestId());
+        fxModel.setTestName(test.getName());
+        fxModel.setCategoryId(CategoryConverter.categoryToCategoryFx(test.getCategoryId()));
+        return fxModel;
     }
 
-    public static Test testFxtoTest()
+    public static Test testFxtoTest(TestFxModel fxModel)
     {
-
+        Test test = new Test();
+        test.setTestId(fxModel.getTestId());
+        test.setCategoryId(CategoryConverter.categoryFxToCategory(fxModel.getCategoryId()));
+        return test;
     }
 }
