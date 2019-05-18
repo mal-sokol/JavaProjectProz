f8c579b7f7be101291bd8ef982da58bed1bd98e1
mal-sokol <malgorzata@e-sokol.pl>
2019-05-18 19:20:00 +0200

    propozycja funkcji logowania

--- src/main/java/proz/models/UserDataModel.java
+++ src/main/java/proz/models/UserDataModel.java
@@ -1,5 +1,27 @@
 package proz.models;
 
+import proz.database.daos.UserDao;
+import proz.database.models.User;
+import proz.utils.exceptions.ApplicationException;
+
+import java.sql.SQLException;
+
 public class UserDataModel
 {
+
+    /*PROPOZYCJA LOGOWANIA*/
+    public boolean loggedAsTeacher(String username, String password) throws ApplicationException, SQLException {
+
+        UserDao userDao = new UserDao();
+        Boolean isLogged = userDao.getQueryBuilder(User.class).where().eq("USERNAME", username).and().eq("PASSWORD", password).and().eq("TEACHER", true).countOf() > 0;
+        return isLogged;
+    }
+
+    public boolean loggedAsStudent(String username, String password) throws ApplicationException, SQLException {
+
+        UserDao userDao = new UserDao();
+        Boolean isLogged = userDao.getQueryBuilder(User.class).where().eq("USERNAME", username).and().eq("PASSWORD", password).countOf() > 0;
+        return isLogged;
+    }
+
 }
